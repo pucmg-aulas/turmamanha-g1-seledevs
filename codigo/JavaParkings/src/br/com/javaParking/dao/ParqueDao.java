07dacc3666bf07b43778f23f5a2960102631c515
Vinicius Gomes <viniciusgomes2202@gmail.com>
2024-11-22 19:11:03 -0300

    Update ParqueDAO.java

--- codigo/JavaParkings/src/br/com/javaParking/dao/ParqueDao.java
+++ codigo/JavaParkings/src/br/com/javaParking/dao/ParqueDao.java
@@ -1,9 +1,15 @@
 package br.com.javaParking.dao;
 
+// IMPORTS DE CLASSES:
 import br.com.javaParking.model.Parque;
 import br.com.javaParking.model.Veiculo;
-import br.com.javaParking.util.Util;
+import br.com.javaParking.util.Comunicacao;
+import br.com.javaParking.controller.ParqueController;
+import br.com.javaParking.model.Configuracao;
 
+
+// IMPORT DE BIBLIOTECAS DO JAVA:
+import br.com.javaParking.util.Util;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -17,52 +23,100 @@ import java.util.List;
 public class ParqueDAO extends ConexaoDAO implements Serializable{
     
     private List<Parque> parques;
+    
     // Atributo da própria classe, estático, para implementar o Singleton
     private static ParqueDAO instance;
 
     //Endereço do arquivo serializado que contém a coleção de veiculos
     private final String localArquivo = Util.CAMINHOPADRAO + "Parques.dat";
 
-    //Construtor privado, pois não podemos permitir mais de uma instância desta classe
-    //que controla a coleção de carros do sistema (Singleton)
+    /*  Construtor privado, pois não podemos permitir mais de uma instância desta classe
+        que controla a coleção de carros do sistema (Singleton)
+    */ 
+    
     private ParqueDAO() {
         this.parques = new ArrayList<>();
         carregaParques();
     }
 
-    //Método para recuperar a única instância de veiculos
-    public static ParqueDAO getInstance() {
+    public static String criarTabelaParques () {
+        try {
+            Comunicacao.setSql  ("""
+                                    CREATE TABLE IF NOT EXISTS 
+                                        interno.tbparques(
+                                            id SERIAL PRIMARY KEY IS NOT NULL,
+                                            nomeParque VARCHAR(100),
+                                            numeroDeVagas INT NOT NULL,
+                                            vagasPorFileira INT NOT NULL,
+                                """);
+            Comunicacao.prepararConexcao();
+            Comunicacao.executar();
+            adicionarParqueInicial();
+
+        } catch (Exception e) {
+            return "Erro ao criar tabela tbparques: " + e;
+        }
+        return "Tabela tbparques criada com sucesso";
+    }
+    
+    private static void adicionarParqueInicial() {
+        try {
+            if (getInstance().buscarTodosParques().isEmpty()) {
+                Comunicacao.setSql  ("""
+                                        INSERT INTO interno.tbparques
+                                            (nomeParque, numeroDeVagas, vagasPorFileira)
+                                        VALUES ('Parque Central', 100, 20);
+                                    """);
+                Comunicacao.prepararConexcao();
+                Comunicacao.executar();
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao adicionar parque inicial: " + e);
+        }
+    }
+    
+    public List<Parque> buscarTodosParques() {
+        List<Parque> listaParques = new ArrayList<>();
+        try {
+            Comunicacao.setSql  ("  SELECT * FROM interno.tbparques;");
+            Comunicacao.prepararConexcao();
+            Comunicacao.executarQuery();
+
+            while (Comunicacao.getRs().next()) {
+                Parque parque = new Parque(
+                    Comunicacao.getRs().getInt("id"),
+                    Comunicacao.getRs().getString("nomeParque"),
+                    Comunicacao.getRs().getInt("numeroDeVagas"),
+                    Comunicacao.getRs().getInt("vagasPorFileira")
+                );
+                listaParques.add(parque);
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao buscar parques: " + e);
+        }
+        return listaParques;
+    }
+    
+    
+    // INICIO DOS MÉTODOS AUXILIARES DO CRUD:
+    
+    public static ParqueDAO getInstance() { //Método para recuperar a única instância de veiculos
         if (instance == null) {
             instance = new ParqueDAO();
         }
         return instance;
     }
 
-    public boolean addParque(Parque parque) {
-        try {
-            this.parques.add(parque);
-            grava();
-            return true;
-        } catch(Exception e) {
-            return false;
-        }
-    }
-
-    private void carregaParques() {
+    public void carregaParques() {
         this.parques = super.leitura(localArquivo);
     }
 
-    private void grava() {
+    public void grava() {
         super.grava(localArquivo, parques);
     }
 
     public List<Parque> getParques() {
         return parques;
-    }
-
-    public void excluirParque(Parque parque) {
-        parques.remove(parque);
-        grava();
     }
 
     public Parque buscarPorNome(String nomeParque) {
@@ -93,25 +147,71 @@ public class ParqueDAO extends ConexaoDAO implements Serializable{
         return resultado;
     }
     
+    // FIM DOS MÉTODOS AUXILIARES DO CRUD.
+    
     /* Nos parametros temos dois: 
             veiculoExistente -> Em termos de codigo ele seria o novo Veiculo/Objeto que vc vai colocar no lugar de um antigo 
                                 (Observação: o novo objeto tem que ter um campo em comum com o antigo)
             placaAnterior -> Seria o campo em comum ultilizado para localizar e substituir
     */
+    
+    // INICIO DOS MÉTODOS ESPECIAIS DE CRUD DO PARQUE:
+    
+    public boolean addParque(Parque parque) {
+        
+        try {
+            Comunicacao.setSql("""
+                                INSERT INTO 
+                               interno.tbparques(
+                                    nomeParque, 
+                                    numeroVagas, 
+                                    vagasPorFileira,)
+                                VALUES (?, ?, ?);
+                                """);
+            Comunicacao.prepararConexcao();
+            Comunicacao.getPst().setString(1, parque.getNomeParque());
+            Comunicacao.getPst().setInt(2, parque.getNumeroVagas());
+            Comunicacao.getPst().setInt(3, parque.getVagasPorFileira());
+            Comunicacao.executar();
+            return true;
+        } catch (Exception e) {
+            System.err.println("Erro ao adicionar parque: " + e);
+            return false;
+        }
+        
+        /*
+        try {
+            this.parques.add(parque);
+            grava();
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    */
+    }
+    
     public boolean alterarParque(Parque parqueSelecionado, int parqueIdentificador) {
         try {
         // Localiza e substitui o parque com o identificador correto
         for (int i = 0; i < parques.size(); i++) {
             if (parques.get(i).getId() == parqueIdentificador) {
-                parques.set(i, parqueSelecionado); // Substitui o parque existente pelo atualizado
-                grava(); // Grava a lista atualizada
-                return true;
+                    parques.set(i, parqueSelecionado); // Substitui o parque existente pelo atualizado
+                    grava(); // Grava a lista atualizada
+                    return true;
+                }
             }
+            return false; // Retorna false se o parque com o identificador não foi encontrado
+        } catch (Exception e) {
+            e.printStackTrace(); // Imprime a pilha de erro para facilitar a depuração
+            return false;
         }
-        return false; // Retorna false se o parque com o identificador não foi encontrado
-    } catch (Exception e) {
-        e.printStackTrace(); // Imprime a pilha de erro para facilitar a depuração
-        return false;
     }
-}
+    
+    public void excluirParque(Parque parque) {
+        parques.remove(parque);
+        grava();
+    }
+    
+    // FIM DOS MÉTODOS ESPECIAIS DE CRUD DO PARQUE:
+    
 }
