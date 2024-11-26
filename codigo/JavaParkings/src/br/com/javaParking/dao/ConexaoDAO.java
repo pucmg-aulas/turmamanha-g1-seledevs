e8ca8c553b1df35360966dd4655994b8e7b054a3
Leandro Alencar <leandro130333.dev@gmail.com>
2024-11-26 20:44:27 -0300

    [FIX] - Conexão

--- codigo/JavaParkings/src/br/com/javaParking/dao/ConexaoDAO.java
+++ codigo/JavaParkings/src/br/com/javaParking/dao/ConexaoDAO.java
@@ -1,116 +1,70 @@
 package br.com.javaParking.dao;
 
-import br.com.javaParking.model.Configuracao;
 import br.com.javaParking.util.Comunicacao;
-import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
-public class ConfiguracaoDAO {
+public class ConexaoDAO {
 
-    public static String criarTabela() {
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+    private static Connection CON;
+    private static Connection conexao = null;
+    private static PreparedStatement pst = null;
+
+    private static Connection ConexaoPadrao() {
+        URL = "jdbc:postgresql://localhost:5432/";
+        USER = "xumlambs";
+        PASSWORD = "123";
+        System.out.println("1");
         try {
-            Comunicacao.setSql("""
-			CREATE TABLE IF NOT EXISTS 
-                               interno.tbconfiguracao(
-                                        id SERIAL,
-					valorPorTempo NUMERIC(1000,2),
-                                        valorMaximoDiaria NUMERIC(1000,2),
-                                        intervaloCobrancaMinutos INT,  
-                                        porcentagemPDC NUMERIC(1000,2),
-                                        porcentagemIdoso NUMERIC(1000,2),
-                                        porcentagemVIP NUMERIC(1000,2),
-                                        PRIMARY KEY (id)
-                               );
-					""");
-            Comunicacao.prepararConexcao();
-            Comunicacao.executar();
-            adicionar();
-
+            Class.forName("org.postgresql.Driver");
+            CON = DriverManager.getConnection(URL, USER, PASSWORD);
+            return CON;
         } catch (Exception e) {
-            return "Erro ao criar tbconfiguracao: " + e;
+            return null;
         }
-        return "tbconfiguracao criada com sucesso";
-    }
-
-    public static Configuracao configuracao() {
-        try {
-
-            Comunicacao.setSql("SELECT * from interno.tbconfiguracao where interno.tbconfiguracao.id = 1;");
-            Comunicacao.prepararConexcao();
-            Comunicacao.executarQuery();
-
-            while (Comunicacao.getRs().next()) {
-                Configuracao x = new Configuracao(
-                        Comunicacao.getRs().getDouble("porcentagemIdoso"),
-                        Comunicacao.getRs().getDouble("porcentagemPDC"),
-                        Comunicacao.getRs().getDouble("porcentagemVIP"),
-                        Comunicacao.getRs().getInt("intervaloCobrancaMinutos"),
-                        Comunicacao.getRs().getDouble("valorMaximoDiaria"),
-                        Comunicacao.getRs().getDouble("valorPorTempo")
-                );
-
-                return x;
-            }
-
-        } catch (Exception e) {
-        }
-
-        return null;
 
     }
 
-    private static void adicionar() {
-        try {
-            if(configuracao() == null){
-             Comunicacao.setSql("""
-				INSERT INTO
-                                        interno.tbconfiguracao
-                                            (valorPorTempo,
-                                             valorMaximoDiaria,
-                                             intervaloCobrancaMinutos,  
-                                             porcentagemPDC,
-                                             porcentagemIdoso,
-                                             porcentagemVIP)
-					VALUES
-                                            (4.00,
-                                             50.00,
-                                             15,
-                                             5,
-                                             10,
-                                             20);
-					""");
-            Comunicacao.prepararConexcao();
-            Comunicacao.executar();
-            }
-           
+    public static Connection Conexao() {
+        URL = "jdbc:postgresql://localhost:5432/dbxumlambs";
+        USER = "xumlambs";
+        PASSWORD = "123";
 
+        System.out.println("2");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            CON = DriverManager.getConnection(URL, USER, PASSWORD);
+            return CON;
         } catch (Exception e) {
+            CriarBanco();
+            return null;
         }
     }
 
-    public static void salvar(Configuracao x) {
+    public static String Tabelas() {
         try {
-            Comunicacao.setSql(""" 
-                               UPDATE
-                                   interno.tbconfiguracao
-            			SET
-            			   valorPorTempo=?,
-                                   valorMaximoDiaria=?,
-                                   intervaloCobrancaMinutos=?,  
-                                   porcentagemPDC=?,
-                                   porcentagemIdoso=?,
-                                   porcentagemVIP=?
-            			WHERE
-            			   id=1
-                                """);
+            Comunicacao.setSql("CREATE SCHEMA IF NOT EXISTS interno");
             Comunicacao.prepararConexcao();
-            Comunicacao.getPst().setDouble(1, x.getValorPeriodoPorTempo());
-            Comunicacao.getPst().setDouble(2, x.getValorMaximoDiaria());
-            Comunicacao.getPst().setInt(3, x.getIntervaloCobrancaMinutos());
-            Comunicacao.getPst().setDouble(4, x.getPorcentagemMinimaPCD());
-            Comunicacao.getPst().setDouble(5, x.getPorcentagemMinimaIdosos());
-            Comunicacao.getPst().setDouble(6, x.getPorcentagemMinimaVIP());
             Comunicacao.executar();
+        } catch (Exception e) {
+            return "Erro ao Criar SchemaInterno: " + e;
+        } finally {
+            ConfiguracaoDAO.criarTabela();
+        }
+        return "";
+    }
 
+    private static void CriarBanco() {
+        try {
+            conexao = ConexaoPadrao();
+            String sqo = "CREATE DATABASE dbxumlambs";
+            pst = CON.prepareStatement(sqo);
+            pst.execute();
         } catch (Exception e) {
         }
     }
